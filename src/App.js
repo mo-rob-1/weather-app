614f0e09fc4807f326bb7aa00d2c976b50192bab
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-01-23 18:03:47 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import DisplayWeather from "./components/DisplayWeather";
-import Cities from "./components/Cities";
-import Footer from "./components/Footer";
+// import Cities from "./components/Cities";
+// import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <div className="container">
-        {/* <Header /> */}
-        <DisplayWeather />
-        {/* <Cities /> */}
-        {/* <Footer /> */}
-      </div>
+      {/* <Header /> */}
+      <DisplayWeather />
+      {/* <Cities /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
