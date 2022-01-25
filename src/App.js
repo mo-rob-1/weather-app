fae0cc118e7f4a71aa60bbc384406bedc751cfb9
Mo <31892724+mo-rob-1@users.noreply.github.com>
2022-01-25 18:30:12 +0000

    remove unused components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-// import Header from "./components/Header";
 import DisplayWeather from "./components/DisplayWeather";
-// import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
       <DisplayWeather />
-      {/* <Footer /> */}
     </div>
   );
 }
